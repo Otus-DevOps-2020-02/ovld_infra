d979edecd3529224ba99559b3c2ae3d8b4455478
vld <vldspinninrecords@gmail.com>
2020-03-19 23:28:17 +0700

    cloud-testapp HW add sudo for manual scripts

--- install_ruby.sh
+++ install_ruby.sh
@@ -5,7 +5,7 @@ PACKAGES="ruby-full ruby-bundler build-essential"
 
 function update() {
     echo 'Download the package lists from the repos'
-    apt update
+    sudo apt update
     if [ $? -ne 0 ]; then
         echo "Download the package lists has failed"
         exit 1
@@ -15,14 +15,14 @@ function update() {
 
 function job() {
     for package in $PACKAGES; do
-        dpkg -s $package > /dev/null 2>&1 && echo "$package is installed" || install $package
+        sudo dpkg -s $package > /dev/null 2>&1 && echo "$package is installed" || install $package
     done
 }
 
 
 function install() {
     echo "Installing package $1"
-    apt install -y $1
+    sudo apt install -y $1
     if [ $? -ne 0 ]; then echo "Install package $1 failed" ; exit 1 ; fi
 }
 
